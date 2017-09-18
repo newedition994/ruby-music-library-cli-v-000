b06789cbb3e1201a33e34b94acdb176ab27559f8
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-18 19:36:00 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
     genre = Genre.find_by_name(input)
     if genre != nil
       genre.songs.sort_by{|song| song.name}.each_with_index do |song, index|
-        
+        puts "#{index + 1}. #{song.artist.name} - #{song.name}"
       end
     end
   end

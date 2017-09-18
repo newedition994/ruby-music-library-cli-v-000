07b0ca6b5caa8fe8eaa51dad35b62973e18d15ac
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-18 19:10:35 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -15,6 +15,12 @@ class MusicLibraryController
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
+      input = gets.strip
+      if input == "list songs"
+        list_songs
+      elsif input == "list artists"
+
+      end
     end
   end
 

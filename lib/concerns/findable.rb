94cd32269a04e055c1b48da9bab1e58c96121c0b
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-18 18:43:38 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,12 @@
 module Concerns::Findable
 
-  def find_by_name
-
+  def find_by_name(name)
+    self.all.each do |item|
+      if item.name == name
+        return item
+      end
+    end
+    return nil
   end
 
   def find_or_create_by_name

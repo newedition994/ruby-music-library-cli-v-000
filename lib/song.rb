9411f7adc4b678d15121ca46b1fd7f3660912260
Breyanna Vass <breyanna.bvass@gmail.com>
2017-09-18 18:30:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,11 +47,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) != nil
-      return self.find_by_name(name)
-    end
-    self.create(name)
-  end
+     if self.find_by_name(name) != nil
+       return self.find_by_name(name)
+     end
+     self.create(name)
+   end
 
 
 end
